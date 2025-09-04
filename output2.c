7cd526fb4aa071bf4e59253533a76dbc799984b8
avnimehtaaaaaa <mehtavni2007@gmail.com>
2025-09-04 20:45:03 +0530

    Add files via upload

---
+++ output2.c
@@ -0,0 +1,7 @@
+
+enter first number:10
+enter second number:2
+sum : 12
+difference : 8
+product : 20
+quotient : 5
---
+++ que2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+  int first,second,sum,diff,prod,quotient;
+  printf("enter first number:");
+  scanf("%d",&first);
+  printf("enter second number:");
+  scanf("%d",&second);
+  sum=first+second;
+  diff=first-second;
+  prod=first*second;
+  quotient=first/second;
+  printf("sum : %d\n",sum);
+  printf("difference : %d\n",diff);
+  printf("product : %d\n",prod);
+  printf("quotient : %d\n",quotient);
+  
+  return 0;
+}
+
+  
+
+
+
+
+
+
+
+
+
