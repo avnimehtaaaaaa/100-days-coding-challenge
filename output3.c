a02846d4a5382901344e3fabcae726fa04f50451
avnimehtaaaaaa <mehtavni2007@gmail.com>
2025-09-04 20:45:41 +0530

    Add files via upload

---
+++ output3.c
@@ -0,0 +1,4 @@
+length of rectangle :5
+breadth of rectangle :10
+area :50
+perimeter :30
---
+++ que3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int length,breadth,area,perimeter;
+printf("length of rectangle :");
+scanf("%d",&length);
+printf("breadth of rectangle :");
+scanf("%d",&breadth);
+area=length*breadth;
+perimeter=2*(length+breadth);
+printf("area :%d\n",area);
+printf("perimeter :%d\n",perimeter);
+return 0;
+}
+
+
+
+
