9147f731748e2a67b187bc80147c6ff03ade1ae6
avnimehtaaaaaa <mehtavni2007@gmail.com>
2025-09-04 20:47:21 +0530

    Add files via upload

---
+++ output5.c
@@ -0,0 +1,3 @@
+enter temperature in celsius ;0
+celsius : 0.000000
+farenhite : 32.000000
---
+++ que5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    float celsius,farenhite;
+  printf("enter temperature in celsius ;");
+  scanf("%f",&celsius);
+  farenhite=(celsius*9/5)+32;
+  printf("celsius : %f\n",celsius);
+  printf("farenhite : %f\n",farenhite);
+  return 0;
+
+
+
+
+}
