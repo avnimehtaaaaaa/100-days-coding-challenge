5dd720ae243bdfd61fa80091c2bab0c2fd5c3eb0
avnimehtaaaaaa <mehtavni2007@gmail.com>
2025-09-05 00:31:27 +0530

    Add files via upload

---
+++ output8.c
@@ -0,0 +1,2 @@
+Enter a positive integer: 5  
+Sum of the first 5 natural numbers = 15
---
+++ que8.c
@@ -0,0 +1,10 @@
+ #include <stdio.h>
+ int main() 
+{
+    int n, sum;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+    sum = n * (n + 1) / 2;
+    printf("Sum of the first %d natural numbers = %d\n", n, sum);
+   return 0;
+}
