95dfe4fd7c5305f5c2ecf4bd0ac5f9cfed7c384d
avnimehtaaaaaa <mehtavni2007@gmail.com>
2025-10-02 00:04:57 +0530

    Add files via upload

---
+++ que37.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, tempA, tempB, gcd, lcm;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    tempA = a;
+    tempB = b;
+
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    gcd = a;
+    lcm = (tempA * tempB) / gcd;
+
+    printf("%d\n", lcm);
+
+    return 0;
+}
